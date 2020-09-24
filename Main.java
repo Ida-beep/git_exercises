89d651c73b840c5889a58cc8a19b3948631a290e
idavill <idamariavilladsen@gmail.com>
2020-09-24 12:45:24 +0200

    add intitial code

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    
+    /**
+     * GIT EXERCISES: 
+     * - go to the directory of you .java.
+     * - type git init in terminal 
+     * 
+     */
+    
+    public static void main (String[] args) {
+        int amountOfCookies = 4;
+        System.out.println("Hello there. I have " + amountOfCookies + " cookies for you!");
+
+    }
+}
